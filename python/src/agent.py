e252ab77922539025ed6a4b0e93700f5f5b7e1bb
cjlovering <cjlovering@wpi.edu>
2020-04-28 22:38:14 -0400

    Switch to Bidding on Campaigns
    * Switch to biddings on campaigns
    * Added a 1-day tier-1 agent
    * Refactored 1-day code to a function
    * Prints out average agent reward (sorted)

--- python/src/agent.py
+++ python/src/agent.py
@@ -1,39 +1,90 @@
 import random
+from collections import defaultdict
+
 
 class Agent(object):
-	def __init__(self, name):
-		self.active_campaigns = []
-		self.profit = 0
-		self.name = name
+    def __init__(self, name):
+        self.active_campaigns = []
+        self.profit = 0
+        self.name = name
 
-	def add_campaign(self, campaign):
-		self.active_campaigns.append(campaign)
+    def reset(self):
+        self.active_campaigns = []
+        self.profit = 0
 
-	def update_campaign(self,user,price):
-		assigned_price = False #To prevent us from double counting costs
-		for campaign in self.active_campaigns:
-			if campaign.matches_user(user) == True:
-				campaign.matching_impressions += 1
-				if assigned_price == False:
-					assigned_price = True
-					campaign.cost += price
+    def add_campaign(self, campaign):
+        self.active_campaigns.append(campaign)
 
-	def calculate_profit(self):
-		for campaign in self.active_campaigns:
-			#TODO: Multi-day: only execute this if the campaign is over
-			self.profit += (campaign.budget * campaign.effective_reach() - campaign.cost)
-			self.active_campaigns.remove(campaign)
+    def update_campaign(self, user, price):
+        for campaign in self.active_campaigns:
+            if campaign.matches_user(user):
+                campaign.matching_impressions += 1
+                campaign.cost += price
+                # To prevent us from double counting costs
+                break
+
+    def calculate_profit(self):
+        for campaign in self.active_campaigns:
+            # TODO: Multi-day: only execute this if the campaign is over
+            self.profit += campaign.budget * campaign.effective_reach() - campaign.cost
+            self.active_campaigns.remove(campaign)
+
+
+class BidBucket:
+    def __init__(self, user, campaign_to_bid, campaign_to_limit):
+        self.user = user
+        self.campaign_to_bid = campaign_to_bid
+        self.campaign_to_limit = campaign_to_limit
+
+    def bid(self, user):
+        campaign_to_bid = {}
+        for campaign in self.user.active_campaigns:
+            if (
+                campaign.matches_user(user)
+                and campaign.cost < self.campaign_to_limit[campaign]
+            ):
+                campaign_to_bid[campaign] = self.campaign_to_bid[campaign]
+            else:
+                campaign_to_bid[campaign] = 0.0
+        return campaign_to_bid
+
 
 class RandomAgent(Agent):
-	def __init__(self, name):
-		super(RandomAgent, self).__init__(name)
+    def __init__(self, name):
+        super(RandomAgent, self).__init__(name)
 
-	def bid_on_user(self, user):
-		relevant = False
-		bid = 0
-		for campaign in self.active_campaigns:
-			if campaign.matches_user(user) == True and campaign.matching_impressions < campaign.reach:
-				relevant = True
-		if relevant == True:
-			bid = random.random()
-		return bid
+    def bid_on_user(self, user):
+        relevant = False
+        bid = 0
+        for campaign in self.active_campaigns:
+            if (
+                campaign.matches_user(user) == True
+                and campaign.matching_impressions < campaign.reach
+            ):
+                relevant = True
+        if relevant == True:
+            bid = random.random()
+        return bid
+
+    def bid_on_campaigns(self):
+        campaign_to_bid = defaultdict(float)
+        campaign_to_limit = defaultdict(float)
+        for campaign in self.active_campaigns:
+            campaign_to_bid[campaign] = random.random()
+            campaign_to_limit[campaign] = campaign.budget
+        # TODO: You can limit both the sub-campaigns and the sup-campaigns.
+        return BidBucket(self, campaign_to_bid, campaign_to_limit)
+
+
+class Tier1Agent(Agent):
+    def __init__(self, name):
+        super(Tier1Agent, self).__init__(name)
+
+    def bid_on_campaigns(self):
+        campaign_to_bid = defaultdict(float)
+        campaign_to_limit = defaultdict(float)
+        for campaign in self.active_campaigns:
+            campaign_to_bid[campaign] = campaign.budget / campaign.reach
+            campaign_to_limit[campaign] = campaign.budget
+        # TODO: You can limit both the sub-campaigns and the sup-campaigns.
+        return BidBucket(self, campaign_to_bid, campaign_to_limit)
--- python/src/main.py
+++ python/src/main.py
@@ -1,83 +1,157 @@
 import itertools
 import copy
 import random
-from agent import RandomAgent
+from agent import RandomAgent, Tier1Agent
 from campaign import Campaign, User, campaigns
 import numpy as np
+import pandas as pd
 
-def second_price_auction(bids):
-	"""
-	input:
-		bids: array of floats corresponding to each player's bids for the auction
 
-	output:
-		winning_bidder: index of highest value from bids. if no one bids it is randomly allotted!!!
-		price_to_pay: second highest bid
-	"""
-	assert all([bid >= 0 for bid in bids])
-	if len(bids) == 1:
-		return bids[0], 0
-	else:
-		highest_bid = max(bids)
-		all_highest_indices = [i for i, j in enumerate(bids) if j == highest_bid]
-		if len(all_highest_indices) > 1:
-			winning_bidder = random.choice(all_highest_indices)
-			return winning_bidder, highest_bid
-		else:
-			winning_bidder = bids.index(highest_bid)
+def second_price_auction(buckets):
+    """
+    input:
+        bids: array of floats corresponding to each player's bids for the auction
 
-			new_list = copy.deepcopy(bids)
+    output:
+        winning_bidder: index of highest value from bids. if no one bids it is randomly allotted!!!
+        price_to_pay: second highest bid
+    """
+    # Each agent makes a bid w.r.t. each campaign.
+    # list[campaign -> bid]
+    bid_values = list(
+        itertools.chain.from_iterable(bucket.values() for bucket in buckets)
+    )
+    agent_ids = list(
+        itertools.chain.from_iterable(
+            [i] * len(bucket.values()) for i, bucket in enumerate(buckets)
+        )
+    )
+    # order of bids smallest to largest
+    order = np.argsort(bid_values)
+    winner = agent_ids[order[-1]]
+    second = bid_values[order[-2]]
+    return winner, second
 
-			# removing the largest element from temp list
-			new_list.remove(highest_bid)
-			# elements in original list are not changed
-			# print(list1)
+    # assert all([bid >= 0 for bid in bid_values])
 
-			price_to_pay = max(new_list)
-			return winning_bidder, price_to_pay
+    # first = 0
+    # second = 0
+    # winner = random.randint(0, len(bids))
+
+    # for index, bid_map in enumerate(bids):
+    #     # let's assume that agent's don't compete with themselves
+    #     # e.g. for different campaigns have different bids which end
+    #     # up being the highest and second highest price.
+    #     biggest = max(bid_map.values())
+    #     if biggest > first:
+    #         second = first
+    #         first = biggest
+    #         winner = index
+    #     elif biggest > second:
+    #         second = biggest
+    # return winner, second
+
+    # bid_values_np = np.array(bid_values)
+
+    # if (bid_values_np > 0).sum() == 1:
+    #     # We have a single winner.
+    #     return
+
+    # if len(bids) == 1:
+    #     return bids[0], 0
+    # else:
+    #     highest_bid = max(bids)
+    #     all_highest_indices = [i for i, j in enumerate(bids) if j == highest_bid]
+    #     if len(all_highest_indices) > 1:
+    #         winning_bidder = random.choice(all_highest_indices)
+    #         return winning_bidder, highest_bid
+    #     else:
+    #         winning_bidder = bids.index(highest_bid)
+
+    #         new_list = copy.deepcopy(bids)
+
+    #         # removing the largest element from temp list
+    #         new_list.remove(highest_bid)
+    #         # elements in original list are not changed
+    #         # print(list1)
+
+    #         price_to_pay = max(new_list)
+    #         return winning_bidder, price_to_pay
+
 
 def sample_user(campaigns):
-	user_demographics = [c for c in campaigns if c.count_attrs == 3]
-	user_distribution = [c.average_users for c in user_demographics]
-	sum_users = sum(user_distribution)
-	user_distribution = [i * 1.0 / sum_users for i in user_distribution]
-	chosen_demographic = random.choices(user_demographics, weights=user_distribution)[0]
-	user = User(attr_gender = chosen_demographic.attr_gender,
-	 			attr_age = chosen_demographic.attr_age,
-				attr_income = chosen_demographic.attr_income)
-	return user
+    user_demographics = [c for c in campaigns if c.count_attrs == 3]
+    user_distribution = [c.average_users for c in user_demographics]
+    sum_users = sum(user_distribution)
+    user_distribution = [i * 1.0 / sum_users for i in user_distribution]
+    chosen_demographic = random.choices(user_demographics, weights=user_distribution)[0]
+    user = User(
+        attr_gender=chosen_demographic.attr_gender,
+        attr_age=chosen_demographic.attr_age,
+        attr_income=chosen_demographic.attr_income,
+    )
+    return user
+
 
 def generate_campaign(first_day=False):
-	"""
-	output:
-		campaign: Randomly generated campaign object, without budget (bidders must bid).
-	"""
-	campaign = copy.deepcopy(random.choice(campaigns))
-	reach_factor = random.choice([.3, .5, .7])
-	days = random.choice([1])
-	campaign.length = days
-	campaign.reach = days * reach_factor * campaign.average_users
-	if first_day== True:
-		campaign.budget = campaign.reach
-	return campaign
+    """
+    output:
+        campaign: Randomly generated campaign object, without budget (bidders must bid).
+    """
+    campaign = copy.deepcopy(random.choice(campaigns))
+    reach_factor = random.choice([0.3, 0.5, 0.7])
+    days = random.choice([1])
+    campaign.length = days
+    campaign.reach = days * reach_factor * campaign.average_users
+    if first_day == True:
+        campaign.budget = campaign.reach
+    return campaign
+
+
+def get_all_random():
+    agents = [RandomAgent("RandomAgent{}".format(i)) for i in range(0, 10)]
+    return agents
+
+
+def get_one_tier1():
+    agents = [RandomAgent("RandomAgent{}".format(i)) for i in range(0, 9)] + [
+        Tier1Agent("Tier1Agent 1")
+    ]
+    return agents
+
+
+def get_all_tier1():
+    agents = [Tier1Agent("Tier1Agent {}".format(i)) for i in range(0, 10)]
+    return agents
+
+
+def run_1_day(agents):
+    for agent in agents:
+        agent.reset()
+        agent.add_campaign(generate_campaign(first_day=True))
+
+    bid_buckets = [agent.bid_on_campaigns() for agent in agents]
+    for _ in range(0, 10000):
+        user = sample_user(campaigns)
+        bids = [bucket.bid(user) for bucket in bid_buckets]
+        # if sum(bids) == 0:
+        # 	print(user.tostr())
+        # 	print([a.active_campaigns[0].matches_user(user) for a in agents])
+        winning_agent_idx, price = second_price_auction(bids)
+        agents[winning_agent_idx].update_campaign(user, price)
+    # print(sum([a.active_campaigns[0].matching_impressions for a in agents]))
+    # print([a.active_campaigns[0].effective_reach() for a in agents])
+    for a in agents:
+        a.calculate_profit()
+        # print(a.name, a.profit)
+    # best_agent = np.argmax([a.profit for a in agents])
+    # print("{} wins!".format(agents[best_agent].name))
+
+    return {a.name: a.profit for a in agents}
+
 
 if __name__ == "__main__":
-	agents = [RandomAgent("RandomAgent{}".format(i)) for i in range(0, 10)]
-	for agent in agents:
-		agent.add_campaign(generate_campaign(first_day=True))
-
-	for user_idx in range(0,10000):
-		user = sample_user(campaigns)
-		bids = [agent.bid_on_user(user) for agent in agents]
-		#if sum(bids) == 0:
-		#	print(user.tostr())
-		#	print([a.active_campaigns[0].matches_user(user) for a in agents])
-		winning_agent_idx, price = second_price_auction(bids)
-		agents[winning_agent_idx].update_campaign(user, price)
-	print(sum([a.active_campaigns[0].matching_impressions for a in agents]))
-	print([a.active_campaigns[0].effective_reach() for a in agents])
-	for a in agents:
-		 a.calculate_profit()
-		 print(a.name, a.profit)
-	best_agent = np.argmax([a.profit for a in agents])
-	print("{} wins!".format(agents[best_agent].name))
+    agents = get_all_tier1()
+    output = pd.DataFrame([run_1_day(agents) for _ in range(100)])
+    # print out agents by order of average
+    print(output.reindex(output.mean().sort_values().index, axis=1).mean())
